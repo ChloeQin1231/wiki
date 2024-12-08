a89d24889631e2a50636408071a841868ef22827
Qin Chloe <qinchloe@MacBook-Pro-4.local>
2024-12-08 17:59:34 -0500

    feat: modify baseUrl in axios to read config file

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,8 +4,12 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import axios from 'axios';
 
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
 
 
+console.log('env：', process.env.NODE_ENV);
+console.log('server：', process.env.VUE_APP_SERVER);
