418d783178d9e00e1126defdad031149f406894e
Qin Chloe <qinchloe@MacBook-Pro-4.local>
2024-12-07 20:38:59 -0500

    feat: add header and footer components

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <a-layout-footer style="text-align: center">
+  Chloe
+  </a-layout-footer>
+</template>
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
