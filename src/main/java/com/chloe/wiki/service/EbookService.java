ceda20fef1960b204c78fcd767a17e8af6ba6849
Qin Chloe <qinchloe@MacBook-Pro-4.local>
2024-12-08 18:36:11 -0500

    feat: add filters, print interface time consuming

--- src/main/java/com/chloe/wiki/service/EbookService.java
+++ src/main/java/com/chloe/wiki/service/EbookService.java
@@ -9,6 +9,7 @@ import com.chloe.wiki.util.CopyUtil;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +24,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         // copy list
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
